243be82944bf2575a005301ebe00e1d0a22ef883
Bo Rydberg <bolry@hotmail.com>
2017-12-22 19:23:54 +0200

    Add solution to day 7 part 1

--- 2017/day07/day07a_main.cpp
+++ 2017/day07/day07a_main.cpp
@@ -1,74 +1,31 @@
 #include "common.h"
 
-struct Name {
-	std::string name;
-};
-
-namespace std{
- template<> struct hash<Name>{
-	 typedef Name argument_type;
-	 typedef std::size_t result_type;
-	 result_type operator()(argument_type const & n) const noexcept{
-		 return std::hash<std::string>{}(n.name);
-	 }
- }
-}
-
 using namespace std;
 
-bool operator==(const Name& lhs, const Name& rhs){
-	return lhs.name == rhs.name;
-}
-
-using Children = set<Name>;
-using Holder = unordered_map<Name, Children>;
-
-void find_better_root(Name const& name, Holder const& holder, Name* root) {
-	for (auto const& p : holder) {
-//		if (p.second.name.count(name.name)) {
-//			*root.name = p.first;
-//			return;
-//		}
-	}
-}
-
 int main() {
-	Name root;
+	using Children = unordered_map<string, string>;
+	Children children;
 	string line;
-	Holder hmm;
+	string parent;
+	string ignore_weight;
+	string arrow;
+	string child;
 	while (getline(cin, line)) {
 		istringstream iss(line);
-		Name name;
-		int weight;
-		char ch;
-		iss >> name >> ch >> weight >> ch;
-		string arrow;
+		iss >> parent >> ignore_weight;
+		children[parent];
 		if (iss >> arrow) {
-			Children aboves;
-			for (Name above; iss >> above;) {
-				if (above.back() == ',') {
-					above.erase(end(above) - 1);
+			while (iss >> child) {
+				if (child.back() == ',') {
+					child.pop_back();
 				}
-				hmm.erase(above);
-				aboves.insert(above);
-			}
-			find_better_root(name, hmm, &root);
-			hmm.insert(make_pair(name, aboves));
-			if (root.empty()) {
-				root = name;
+				children[child] = parent;
 			}
 		}
 	}
-	while(hmm.size() > 1U){
-		for(auto iter = hmm.begin(); iter != hmm.end(); ++iter){
-			bool erased = false;
-			for( auto child : iter->second){
-				erased = erased || (hmm.erase(child) != 0U);
-			}
-			if(erased){
-				break;
-			}
-		}
-	}
-	cout << "Root program is `" << hmm.begin()->first << "'\n";
+	auto grand_iter = find_if(begin(children), end(children),
+			[](Children::value_type const & c) {
+				return c.second.empty();
+			});
+	cout << "Grand program is " << grand_iter->first << '\n';
 }
