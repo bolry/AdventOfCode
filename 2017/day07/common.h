f4ef0ba0562f102c738b41eccd7570f5f2d8a007
Bo Rydberg <bolry@hotmail.com>
2017-12-22 13:58:35 +0200

    Add non-working day 7

---
+++ 2017/day07/common.h
@@ -0,0 +1,18 @@
+#ifndef SRC_COMMON_H_
+#define SRC_COMMON_H_
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#endif
