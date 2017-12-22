39f54da1bf8c99cc48da5baf85ffe33d48b28b99
Bo Rydberg <bolry@hotmail.com>
2017-12-22 19:36:43 +0200

    Refine comments for day 7 part 1 solution

--- 2017/day07/day07a_main.cpp
+++ 2017/day07/day07a_main.cpp
@@ -3,29 +3,30 @@
 using namespace std;
 
 int main() {
-	using Children = unordered_map<string, string>;
+	// true if child has parent
+	using Children = unordered_map<string, bool>;
 	Children children;
 	string line;
 	string parent;
 	string ignore_weight;
-	string arrow;
+	string ignore_arrow;
 	string child;
 	while (getline(cin, line)) {
 		istringstream iss(line);
 		iss >> parent >> ignore_weight;
-		children[parent];
-		if (iss >> arrow) {
+		children[parent]; // defaults to false if new instance
+		if (iss >> ignore_arrow) {
 			while (iss >> child) {
 				if (child.back() == ',') {
 					child.pop_back();
 				}
-				children[child] = parent;
+				children[child] = true;
 			}
 		}
 	}
 	auto grand_iter = find_if(begin(children), end(children),
 			[](Children::value_type const & c) {
-				return c.second.empty();
+				return c.second == false;
 			});
 	cout << "Grand program is " << grand_iter->first << '\n';
 }
