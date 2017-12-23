0e5067480514d29ee791c0a4d31374ea74bf1f82
Bo Rydberg <bolry@hotmail.com>
2017-12-23 11:39:52 +0200

    Add day 9 step 1 solution

--- 2017/day09/day09a_main.cpp
+++ 2017/day09/day09a_main.cpp
@@ -1,5 +1,41 @@
-#include "common.h"
+#include <iostream>
 
-int main() {
+constexpr char cancel = '!';
+constexpr char garbage_start = '<';
+constexpr char garbage_end = '>';
+constexpr char group_start = '{';
+constexpr char group_end = '}';
 
+int main()
+{
+	char ch;
+	int group_score = 0;
+	int group_level = 0;
+	bool in_garbage = false;
+	bool ignore_next = false;
+	while (std::cin >> ch)
+		if (ignore_next)
+			ignore_next = false;
+		else if (in_garbage)
+			switch (ch) {
+			case garbage_end:
+				in_garbage = false;
+				break;
+			case cancel:
+				ignore_next = true;
+				break;
+			}
+		else
+			switch (ch) {
+			case group_start:
+				group_score += ++group_level;
+				break;
+			case group_end:
+				--group_level;
+				break;
+			case garbage_start:
+				in_garbage = true;
+				break;
+			}
+	std::cout << "Group score " << group_score << '\n';
 }
