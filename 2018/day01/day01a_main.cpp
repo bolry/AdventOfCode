a9dd5444b4af30a4486a3f0ea43bf30f58bac6b7
Bo Rydberg <bolry@hotmail.com>
2018-12-01 14:50:54 +0100

    Refactor for day01a and b

--- 2018/day01/day01a_main.cpp
+++ 2018/day01/day01a_main.cpp
@@ -1,28 +1,34 @@
 #include <cstdio>
-#include <numeric>
-#include <iterator>
-#include <iostream>
-#include <vector>
+#include <cstdlib>
 #include <fstream>
+#include <iostream>
+#include <iterator>
+#include <numeric>
 
 int main(int argc, char *argv[])
 try {
     std::cout.exceptions(std::cout.badbit | std::cout.eofbit | std::cout.failbit);
-    if (argc < 2) throw std::runtime_error("Need input data\n");
-    std::ifstream in(argv[1]);
-    if (!in) throw std::runtime_error(std::string("Unable to open input file: ") + argv[1]);
-    std::vector<int> freq;
-    int value;
-    while (in >> value)
-        freq.push_back(value);
-    std::cout << std::accumulate(begin(freq), end(freq), 0) << '\n' << std::flush;
+    if (argc < 2) throw std::runtime_error("Need input data filename\n");
+    std::ifstream freq_changes_file(argv[1]);
+    if (!freq_changes_file) throw std::runtime_error(std::string("Unable to open input file: ") + argv[1]);
+    std::istream_iterator<int> freq_change_reader(freq_changes_file);
+    std::istream_iterator<int> freq_change_reader_EOF;
+    int const resulting_frequency = std::accumulate(freq_change_reader, freq_change_reader_EOF, 0);
+    std::cout << resulting_frequency << '\n' << std::flush;
+    return EXIT_SUCCESS;
 }
 catch (std::bad_alloc const &) {
     std::fputs("Sorry, out-of-memory\n", stderr);
+    std::fflush(stderr);
+    return EXIT_FAILURE;
 }
-catch (std::exception const &e) {
-    std::cerr << "Got exception: " << e.what() << '\n';
+catch (std::exception const &x) {
+    std::cerr << x.what() << '\n' << std::flush;
+    return EXIT_FAILURE;
 }
+#if 0
 catch (...) {
-    std::cerr << "Unexpected unhandled error\n";
+    std::cerr << "Unexpected unhandled error\n" << std::flush;
+    return EXIT_FAILURE;
 }
+#endif  // 0
