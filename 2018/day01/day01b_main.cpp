ca650e56d8aae186b462f4e5a507b8adc2156c88
Bo Rydberg <bolry@hotmail.com>
2018-12-01 14:50:54 +0100

    Refactor for day01a and b

--- 2018/day01/day01b_main.cpp
+++ 2018/day01/day01b_main.cpp
@@ -1,29 +1,38 @@
 #include <algorithm>
 #include <cstdio>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <iterator>
-#include <numeric>
-#include <unordered_map>
+#include <set>
 #include <vector>
+
+namespace {
+    typedef std::vector<int> IntVec;
+    typedef IntVec::const_iterator VecIter;
+    typedef std::istream_iterator<int> StreamIter;
+}
 
 int main(int argc, char *argv[])
 try {
     std::cout.exceptions(std::cout.badbit | std::cout.eofbit | std::cout.failbit);
     if (argc < 2) throw std::runtime_error("Need input data\n");
-    std::ifstream in(argv[1]);
-    if (!in) throw std::runtime_error(std::string("Unable to open input file: ") + argv[1]);
-    std::vector<int> frequencies;
-    int value;
-    while (in >> value)
-        frequencies.push_back(value);
-    std::unordered_map<int, int> seen;
-    int init = 0;
-    while(true) {
-        for (auto first = begin(frequencies), last = end(frequencies); first != last; ++first) {
-            init += *first;
-            if (seen[init]++) {
-                std::cout << init << '\n' << std::flush;
+    std::ifstream freq_change_file(argv[1]);
+    if (!freq_change_file) throw std::runtime_error(std::string("Unable to open input file: ") + argv[1]);
+    IntVec frequency_change_list;
+    std::copy(StreamIter(freq_change_file), StreamIter(),
+              std::back_insert_iterator<IntVec>(frequency_change_list));
+    std::set<int> seen_frequencies;
+    int current_frequency = 0;
+    VecIter const last = frequency_change_list.end();
+    while (true) {
+        VecIter first = frequency_change_list.begin();
+        for (; first != last; ++first) {
+            current_frequency += *first;
+            bool const isReusedFrequency = !seen_frequencies.insert(current_frequency).second;
+            if (isReusedFrequency) {
+                int const &duplicate_frequency = current_frequency;
+                std::cout << duplicate_frequency << '\n' << std::flush;
                 return EXIT_SUCCESS;
             }
         }
