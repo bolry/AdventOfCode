5ef9641a95948cd65877300840259b80b6e6d616
Bo Rydberg <bolry@hotmail.com>
2018-12-03 23:43:44 +0100

    Add solution to 2018 day 3 first part
    Some work on 2017 day 7

--- 2017/day07/day07b_main.cpp
+++ 2017/day07/day07b_main.cpp
@@ -7,34 +7,58 @@
 #include <iterator>
 #include <map>
 #include <new>
+#include <sstream>
 #include <vector>
 
 namespace {
-struct Program;
-typedef std::vector<Program> ChildPrograms;
+
+typedef std::vector<std::string> StrVec;
+
 struct Program {
   std::string name;
   int weight;
-  ChildPrograms children;
+  StrVec children_names;
 };
+
 std::istream &operator>>(std::istream &in, Program &prog) {
-  return std::getline(in, prog.name);
+  prog.children_names.clear();
+  std::string line;
+  if (!std::getline(in, line)) return in;
+  std::istringstream iss(line);
+  char left_parenthesis = '\0';
+  char right_parenthesis = '\0';
+  iss >> prog.name >> left_parenthesis >> prog.weight >> right_parenthesis;
+  if (!iss || left_parenthesis != '(' || right_parenthesis != ')') return in.setstate(in.failbit), in;
+  std::string arrow;
+  iss >> arrow;
+  if (iss.eof()) return in;
+  if (!iss || arrow != "->") return in.setstate(in.failbit), in;
+  std::string child_name;
+  while (iss >> child_name) {
+    if (child_name.back() == ',') child_name.pop_back();
+    prog.children_names.push_back(child_name);
+  }
+  return in;
 }
+
 std::ostream &operator<<(std::ostream &out, Program const &prog) {
-  return out << "\"Name\": `" << prog.name << '\'';
+  return out << "Name: `" << prog.name << "' Weight: " << prog.weight << " NrChilds: " << prog.children_names.size();
 }
-}
+
+}  // close anonymous namespace
 
 int main(int argc, char *argv[]) try {
   std::cout.exceptions(std::cout.badbit | std::cout.eofbit | std::cout.failbit);
   if (argc < 2) throw std::runtime_error("Sorry, program needs one command line argument");
   std::ifstream disc_data(argv[1]);
   if (!disc_data) throw std::runtime_error(std::string("Unable to open file `") + argv[1] + '\'');
-  ChildPrograms all_progs;
+  std::vector<Program> all_progs;
   std::copy(std::istream_iterator<Program>(disc_data),
             std::istream_iterator<Program>(),
-            std::back_insert_iterator<ChildPrograms>(all_progs));
-  std::copy(all_progs.begin(), all_progs.end(), std::ostream_iterator<Program>(std::cout, "\n"));
+            std::back_inserter(all_progs));
+  std::for_each(all_progs.begin(), all_progs.end(), [&all_progs](Program &prog) {
+    prog.children_names;
+  });
   return EXIT_SUCCESS;
 }
 catch (std::bad_alloc const &) {
