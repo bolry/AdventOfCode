e7f940d3b2f00021df6e7cec22e2a673af2d06e6
Bo Rydberg <bolry@hotmail.com>
2018-12-04 00:03:12 +0100

    Add 2018 day 3 part 2 solution

--- 2018/day03/day03b_main.cpp
+++ 2018/day03/day03b_main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdio>
 #include <cstdlib>
 #include <exception>
@@ -6,19 +7,86 @@
 #include <iterator>
 #include <new>
 #include <string>
+#include <vector>
 
-namespace {}
+namespace {
+
+typedef std::vector<int> Row;
+
+struct Claim {
+  int id;
+  int left_offset;
+  int top_offset;
+  int width;
+  int height;
+};
+
+std::istream &operator>>(std::istream &in, Claim &claim) {
+  char hash, comma, colon, x;
+  std::string at;
+  return in >> hash >> claim.id >> at >> claim.left_offset >> comma >> claim.top_offset >> colon >> claim.width >> x
+            >> claim.height;
+}
+
+void cover_claim(Claim const &claim, std::vector<Row> &fabric) {
+  int const col_first = claim.left_offset;
+  int const col_last = col_first + claim.width;
+  for (int i = claim.top_offset; i < claim.top_offset + claim.height; i++) {
+    auto row_begin = fabric.at(i).begin();
+    std::for_each(row_begin + col_first, row_begin + col_last, [](int &n) { n++; });
+  }
+}
+
+bool only_claimer(Claim const &claim, std::vector<Row> const &fabric) {
+  int const col_first = claim.left_offset;
+  int const col_last = col_first + claim.width;
+  for (int i = claim.top_offset; i < claim.top_offset + claim.height; i++) {
+    auto row_begin = fabric.at(i).begin();
+    if (std::any_of(row_begin + col_first, row_begin + col_last, [](int const n) { return n > 1; }))
+      return false;
+  }
+  return true;
+}
+
+}
 
 int main(int argc, char *argv[])
 try {
   std::cout.exceptions(std::cout.badbit | std::cout.eofbit | std::cout.failbit);
   if (argc < 2) throw std::runtime_error("Need input data filename\n");
-  std::ifstream in_file(argv[1]);
-  if (!in_file) throw std::runtime_error(std::string("Unable to open input file: ") + argv[1]);
-  std::istream_iterator<std::string> in_reader(in_file);
-  std::istream_iterator<std::string> in_reader_EOF;
-  std::cout << "result b" << '\n' << std::flush;
-  return EXIT_SUCCESS;
+  std::ifstream claim_file(argv[1]);
+  if (!claim_file) throw std::runtime_error(std::string("Unable to open input file: ") + argv[1]);
+  std::istream_iterator<Claim> claim_reader(claim_file);
+  std::istream_iterator<Claim> claim_reader_EOF;
+  std::vector<Claim> claims;
+  std::copy(claim_reader, claim_reader_EOF, std::back_inserter(claims));
+  auto max_width_claim = std::max_element(claims.begin(), claims.end(), [](Claim const &a, Claim const &b) {
+    return (a.left_offset + a.width) < (b.left_offset + b.width);
+  });
+  std::cout << "line nr:" << std::distance(claims.begin(), max_width_claim) + 1 << '\n';
+  std::cout << "Big width (leftoffset):" << max_width_claim->left_offset << " (width):" << max_width_claim->width
+            << '\n';
+  auto max_height_claim = std::max_element(claims.begin(), claims.end(), [](Claim const &a, Claim const &b) {
+    return (a.top_offset + a.height) < (b.top_offset + b.height);
+  });
+  std::cout << "line nr:" << std::distance(claims.begin(), max_height_claim) + 1 << '\n';
+  std::cout << "Big height (top_offset):" << max_height_claim->top_offset << " (height):" << max_height_claim->height
+            << '\n';
+  int const fabric_width = max_width_claim->left_offset + max_width_claim->width;
+  int const fabric_height = max_height_claim->top_offset + max_height_claim->height;
+  std::cout << "fabric width: " << fabric_width << " fabric height: " << fabric_height << '\n';
+  std::vector<Row> fabric(fabric_height, Row(fabric_width));
+  for (Claim const &claim : claims) {
+    cover_claim(claim, fabric);
+  }
+  for (Claim const &claim : claims) {
+    if (only_claimer(claim, fabric)) {
+      std::cout << "Single claim ID is " << claim.id << '\n' << std::flush;
+      return EXIT_SUCCESS;
+    }
+  }
+  std::cout << "Oops, didn't find any single claim\n" << std::flush;
+  return EXIT_FAILURE;
 }
 catch (std::bad_alloc const &) {
   std::fputs("Sorry, out-of-memory\n", stderr);
