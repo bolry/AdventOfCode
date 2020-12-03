39de0232a798bd276f7433996c8844997d72e461
Bo Rydberg <bolry@hotmail.com>
2020-12-03 21:57:02 +0100

    Add 2020 day 3 solutions

--- 2020/day03/day03b.m.cpp
+++ 2020/day03/day03b.m.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstdint>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -8,23 +9,23 @@
 
 namespace
 {
-struct slope
+struct Slope
 {
-   int right;
-   int down = 1;
+   std::size_t right{};
+   std::size_t down{1u};
 };
 }  // namespace
 
-int getTrees(std::vector<std::string> const& map, int right, int down)
+std::uintmax_t getTrees(
+   std::vector<std::string> const& map,
+   std::size_t right,
+   std::size_t down)
 {
-   int treeCnt{};
-   int col{};
-   for(int rowId = 0; rowId < ssize(map); rowId += down)
+   std::uintmax_t treeCnt{};
+   std::size_t col{};
+   for(std::size_t rowId{0u}, rMax{size(map)}; rowId < rMax; rowId += down)
    {
-      if(col >= ssize(map[rowId]))
-      {
-         col %= ssize(map[rowId]);
-      }
+      col %= size(map[rowId]);
       if(map[rowId][col] == '#')
       {
          ++treeCnt;
@@ -37,16 +38,19 @@ int main()
 {
    std::ifstream in("../input.txt");
    std::vector<std::string> map;
-   map.reserve(323);
+   map.reserve(323u);
    std::copy(
       std::istream_iterator<std::string>(in),
       std::istream_iterator<std::string>(),
       std::back_inserter(map));
-   std::array<slope, 5> slopes{{{1}, {3}, {5}, {7}, {1, 2}}};
-   int answer = std::accumulate(
-      cbegin(slopes), cend(slopes), 1, [&map](int a, slope const& b) {
+   std::array<Slope, 5u> slopes{{{1u}, {3u}, {5u}, {7u}, {1u, 2u}}};
+   auto answer = std::accumulate(
+      cbegin(slopes),
+      cend(slopes),
+      std::uintmax_t{1u},
+      [&map](auto a, Slope const& b) {
          return a * getTrees(map, b.right, b.down);
       });
-   std::cout << "Found " << answer << " trees\n" << std::flush;
+   std::cout << "Key multiplicative is " << answer << std::endl;
    return EXIT_SUCCESS;
 }
