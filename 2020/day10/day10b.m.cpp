23ff044c674165d28238a732150b5a6cf5ebe0cd
Bo Rydberg <bolry@hotmail.com>
2020-12-11 00:09:36 +0100

    Solidify 2020 day 10 part 2 solution

--- 2020/day10/day10b.m.cpp
+++ 2020/day10/day10b.m.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <array>
 #include <fstream>
 #include <iostream>
 #include <iterator>
@@ -8,15 +9,28 @@
 using namespace std::literals;
 using namespace std::ranges;
 
+namespace
+{
+constexpr std::array kTranslate{1, 1, 2, 4, 7};
+}
+
 int main()
 {
    std::ifstream in("../input.txt"s);
-   std::vector<int> vec(
-      std::istream_iterator<int>{in}, std::istream_iterator<int>{});
+   using inIter = std::istream_iterator<int>;
+   std::vector vec(inIter{in}, inIter{});
    sort(vec);
    vec.push_back(vec.back() + 3);
    adjacent_difference(cbegin(vec), cend(vec), begin(vec));
-   copy(vec, std::ostream_iterator<int>(std::cout, " "));
-   std::cout << std::flush;
+   std::vector<std::uint_fast8_t> ones;
+   for(auto b{cbegin(vec)}, i{b}, e{cend(vec)}; (i = find(i, e, 3)) != e;
+       b = i = find(i, e, 1))
+      ones.push_back(kTranslate[distance(b, i)]);
+   std::cout << reduce(
+                   cbegin(ones),
+                   cend(ones),
+                   std::uintmax_t{1},
+                   std::multiplies<>{})
+             << std::endl;
    return EXIT_SUCCESS;
 }
