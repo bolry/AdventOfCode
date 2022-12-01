eb9529860fcaa0625c7c058d7a6750a79aa48f81
Bo Rydberg <2945606+bolry@users.noreply.github.com>
2022-12-01 08:31:31 +0100

    2022 day 1

---
+++ 2022/d01/aoc2022d01a.m.cpp
@@ -0,0 +1,27 @@
+int main()
+{
+   std::ifstream ifs("../input.txt");
+   assert(ifs);
+   std::string line;
+   int maxCalories{};
+   int calories{};
+   while(std::getline(ifs, line)) [[likely]]
+   {
+      if(line.empty()) [[unlikely]]
+      {
+         maxCalories = std::max(maxCalories, calories);
+         calories    = 0;
+      }
+      else
+      {
+         int cal;
+         auto [_, ec] = std::from_chars(
+            line.data(), line.data() + line.size(), cal);
+         assert(ec == std::errc{});
+         calories += cal;
+      }
+   }
+   maxCalories = std::max(maxCalories, calories);
+   std::cout << maxCalories << '\n';
+   return EXIT_SUCCESS;
+}
---
+++ 2022/d01/aoc2022d01b.m.cpp
@@ -0,0 +1,29 @@
+int main()
+{
+   std::ifstream ifs("../input.txt");
+   assert(ifs);
+   std::string line;
+   std::vector<int> carrying;
+   carrying.reserve(3000);
+   int calories{};
+   while(std::getline(ifs, line)) [[likely]]
+   {
+      if(line.empty()) [[unlikely]]
+      {
+         carrying.push_back(calories);
+         calories = 0;
+      }
+      else
+      {
+         int cal;
+         auto [_, ec] = std::from_chars(
+            line.data(), line.data() + line.size(), cal);
+         assert(ec == std::errc{});
+         calories += cal;
+      }
+   }
+   carrying.push_back(calories);
+   auto b = carrying.begin();
+   std::ranges::nth_element(carrying, next(b, 2), std::greater<>());
+   std::cout << std::accumulate(b, next(b, 3), int{}) << '\n';
+}
