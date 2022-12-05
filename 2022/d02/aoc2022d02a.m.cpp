783ed6686b7ea48a1ca7273ace5ec38556dca234
Bo Rydberg <2945606+bolry@users.noreply.github.com>
2022-12-05 22:52:17 +0100

    Add 2022 day 2 solution

--- 2022/d02/aoc2022d02a.m.cpp
+++ 2022/d02/aoc2022d02a.m.cpp
@@ -1,56 +1,61 @@
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
 
-namespace {
-    enum Shape {
-        Rock = 1, Paper, Scissors,
-    };
+namespace
+{
+enum Shape
+{
+   Rock = 1,
+   Paper,
+   Scissors,
+};
 
-    constexpr Shape isFig(char ch) noexcept {
-        if (ch == 'A' or ch == 'X') return Rock;
-        else if (ch == 'B' or ch == 'Y') return Paper;
-        else /*if (ch == 'C' or ch == 'Z')*/ return Scissors;
-    }
-
-    constexpr int myScore(char me, char other) noexcept {
-        auto const myFig = isFig(me);
-        auto const otherFig = isFig(other);
-        // tie
-        if (myFig == otherFig) return 3 + myFig;
-        // I win
-        else if ((myFig == Rock and otherFig == Scissors) or
-                 (myFig == Scissors and otherFig == Paper) or
-                 (myFig == Paper and otherFig == Rock))
-            return 6 + myFig;
-        // I lose
-        else return myFig;
-    }
+constexpr Shape toFig(char ch) noexcept
+{
+   if(ch == 'A' or ch == 'X')
+      return Rock;
+   else if(ch == 'B' or ch == 'Y')
+      return Paper;
+   else /*if (ch == 'C' or ch == 'Z')*/
+      return Scissors;
 }
 
-int main() {
-    std::ifstream ifs("../input.txt");
-    assert(ifs);
-
-    int sum{};
-    for (std::string line; std::getline(ifs, line);) {
-        assert(line.size() == 3);
-        int s = myScore(line[2], line[0]);
-        std::cout << '`' << line << "' score: " << s << '\n';
-        sum+=s;
-    }
-    std::cout << "\nTotal score " << sum << '\n';
-
-    return EXIT_SUCCESS;
+constexpr int roundScore(char me, char other) noexcept
+{
+   auto const myFig    = toFig(me);
+   auto const otherFig = toFig(other);
+   // tie
+   if(myFig == otherFig)
+      return 3 + myFig;
+   // I win
+   else if(
+      (myFig == Rock and otherFig == Scissors) or
+      (myFig == Scissors and otherFig == Paper) or
+      (myFig == Paper and otherFig == Rock))
+      return 6 + myFig;
+   // I lose
+   else
+      return myFig;
 }
+}  // namespace
 
-/*
+int main()
+{
+   std::ifstream ifs("../input.txt");
+   assert(ifs);
 
-A X
-A Y
-A Z
-B X
-B Y
-B Z
-C X
-C Y
-C Z
+   int sum{};
+   for(std::string line; std::getline(ifs, line);)
+   {
+      assert(line.size() == 3);
+      int s = roundScore(line[2], line[0]);
+      std::cout << '`' << line << "' score: " << s << '\n';
+      sum += s;
+   }
+   std::cout << "\nTotal score " << sum << '\n';
 
- */
+   return EXIT_SUCCESS;
+}
--- 2022/d02/aoc2022d02bAlt.m.cpp
+++ 2022/d02/aoc2022d02bAlt.m.cpp
@@ -1,36 +1,31 @@
-int main() {
-    std::ifstream ifs("../input.txt");
-    assert(ifs);
-    using namespace std::string_view_literals;
-    int sum{};
-    for (std::string line; std::getline(ifs, line);) {
-        int s;
-        if (line == "A X"sv) s = 3;
-        else if (line == "A Y"sv) s = 4;
-        else if (line == "A Z"sv) s = 8;
-        else if (line == "B X"sv) s = 1;
-        else if (line == "B Y"sv) s = 5;
-        else if (line == "B Z"sv) s = 9;
-        else if (line == "C X"sv) s = 2;
-        else if (line == "C Y"sv) s = 6;
-        else /*if (line == "C Z"sv)*/ s = 7;
-        sum += s;
-    }
-    std::cout << "\nTotal score " << sum << '\n';
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
 
-    return EXIT_SUCCESS;
+int main()
+{
+   std::ifstream ifs("../input.txt");
+   assert(ifs);
+   int sum{};
+   for(std::string line; std::getline(ifs, line);)
+   {
+      int s;
+      // clang-format off
+      if(line == "A X") s = 3;
+      else if(line == "A Y") s = 4;
+      else if(line == "A Z") s = 8;
+      else if(line == "B X") s = 1;
+      else if(line == "B Y") s = 5;
+      else if(line == "B Z") s = 9;
+      else if(line == "C X") s = 2;
+      else if(line == "C Y") s = 6;
+      else /*if (line == "C Z")*/ s = 7;
+      // clang-format on
+      sum += s;
+   }
+   std::cout << "\nTotal score " << sum << '\n';
+
+   return EXIT_SUCCESS;
 }
-
-/*
-
-A X
-A Y
-A Z
-B X
-B Y
-B Z
-C X
-C Y
-C Z
-
- */
