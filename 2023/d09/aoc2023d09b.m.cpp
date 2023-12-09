9b18e9865d5eee3710492a6b6726f6ceb9a1e9bf
Bo Rydberg <2945606+bolry@users.noreply.github.com>
2023-12-09 10:01:01 +0100

    Add 2023 day 9 solution

---
+++ 2023/d09/aoc2023d09b.m.cpp
@@ -0,0 +1,69 @@
+// https://adventofcode.com/2023/day/9#part2
+
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace {
+    namespace r4s = std::ranges;
+    using S = std::intmax_t;
+
+    struct History {
+        std::vector<S> sample;
+
+        friend std::istream& operator>>(std::istream& in, History& history) {
+            if (std::string line; std::getline(in, line)) {
+                std::istringstream iss{std::move(line)};
+                history.sample.assign(std::istream_iterator<S>{iss}, {});
+            }
+            return in;
+        }
+    };
+
+    auto calcDiff = [](std::vector<S> const& in, std::vector<S>& out) {
+        out.resize(in.size());
+        (void) std::adjacent_difference(in.begin(), in.end(), out.begin());
+    };
+
+    S prevValue(History hist) {
+        auto& diffs(hist.sample);
+        std::vector<S> firsts;
+        std::vector<S> tmp;
+        do {
+            calcDiff(diffs, tmp);
+            // for(auto v : tmp) std::cout << v << ' '; std::cout << '\n';
+            firsts.push_back(tmp.front());
+            diffs.assign(std::next(tmp.begin()), tmp.end());
+        } while (r4s::any_of(diffs, [](auto v) { return v != 0; }));
+       // for(auto v:firsts) std::cout << v << ' ' ; std::cout <<'\n';
+        S res{};
+        while(!firsts.empty()) {
+            res = firsts.back() - res;
+            firsts.pop_back();
+        }
+        return res;
+    }
+}
+
+int main([[maybe_unused]] int argc, char* argv[]) {
+    assert(argc == 2);
+    std::ifstream ifs{argv[1]};
+    assert(ifs);
+
+    auto const sum = std::accumulate(std::istream_iterator<History>{ifs}, {}, S{}, [](S acc, auto const& hist) {
+        return acc + prevValue(hist);
+    });
+
+    std::cout << "The sum of the extrapolated values is " << sum << '\n';
+
+    return EXIT_SUCCESS;
+}
